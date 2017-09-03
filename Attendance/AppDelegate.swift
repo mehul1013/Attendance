822ae454a34c380e373eacf839a0aa2d85938e54
MeHuLa <mehulkumars@gmail.com>
2017-09-03 15:55:01 +0530

    Just for testing about commit

--- Attendance/AppDelegate.swift
+++ Attendance/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
+    //MARK: - Application Life Cycle
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
